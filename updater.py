a21cbe6d85f351c989cde96ff43d8ddc7f3957d7
Saksham Singh <sakshamsingh.93502@gmail.com>
2024-12-09 15:22:00 +0530

    path update

--- updater.py
+++ updater.py
@@ -13,19 +13,19 @@ def download():
     link_0 = "https://github.com/Genymobile/scrcpy/releases/latest/download/scrcpy-win64-"
     link_1 = ".zip"
     end_link = link_0+version+link_1
-    final = 'C:\\Tools\\Scrcpy\\scrcpy.zip'
+    final = 'S:\\Tools\\Scrcpy\\scrcpy.zip'
     wget.download(end_link, final)
     return final
 
 
 def extract():
-    with zp('C:\\Tools\\Scrcpy\\scrcpy.zip', 'r') as zipObj:
-        zipObj.extractall('C:\\Tools\\Scrcpy')
+    with zp('S:\\Tools\\Scrcpy\\scrcpy.zip', 'r') as zipObj:
+        zipObj.extractall('S:\\Tools\\Scrcpy')
 
 
 def folder_loc():
     process = subprocess.Popen(
-        ["powershell", "cd C:\\Tools\\Scrcpy\\ ; ls"], stdout=subprocess.PIPE)
+        ["powershell", "cd S:\\Tools\\Scrcpy\\ ; ls"], stdout=subprocess.PIPE)
     result = process.communicate()[0]
     loc = result.decode('utf-8').split()
     folder_name = loc[14]
@@ -33,8 +33,8 @@ def folder_loc():
 
 
 def move(folder_name):
-    des = 'C:\\Tools\\Scrcpy\\'
-    fol_loc = 'C:\\Tools\\Scrcpy\\'+folder_name
+    des = 'S:\\Tools\\Scrcpy\\'
+    fol_loc = 'S:\\Tools\\Scrcpy\\'+folder_name
     file = os.listdir(fol_loc)
     for f in file:
         src = os.path.join(fol_loc, f)
@@ -49,7 +49,7 @@ def remove(fol_loc, final):
 
 
 def folder_check():
-    folder_path = "C:\\Tools\\Scrcpy"
+    folder_path = "S:\\Tools\\Scrcpy"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
